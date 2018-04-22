17a04e5cea4bd9c718c341d4eabae513f60e6fb1
tabbatharenea@gmail.com <Renea415!>
2018-04-22 16:02:14 -0400

    updated project thumbnails, removed about page

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,6 +13,7 @@ $("#closeProjectWindow").click(function(){
   setTimeout(function(){
     $(".projectInfo").css("display", "none");
     $(".projectInfo").removeClass("fadeOut");
+    $("#viewOnWeb").css("display", "block");
   }, 550);
 }); 
 
@@ -24,11 +25,26 @@ $("#rockpaper").click(function(){
   setTimeout(function(){
     $(".projectInfo").addClass("moveInRight");
     $(".projectInfo").css("display", "flex");
-    $(".projectInfo").css("background-size", "cover");
     $("#projectInfoHeader").text("Rock Paper Scissors");
     $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/rock-paper-scissors");
     $("#viewOnWeb").attr("href", "https://tabbplomaritas.github.io/rock-paper-scissors/");
     $("#projectDescript").text("A classic game of Rock, Paper, Scissors using Javascript and jQuery. Includes fun CSS transitions, animations, and sound effects!");  
+  }, 500); 
+
+  setTimeout(function(){
+    $(".projectInfo").removeClass("moveInRight");
+  }, 1000);
+});
+
+//portfolio
+$("#portfolioThumb").click(function(){
+  setTimeout(function(){
+    $(".projectInfo").addClass("moveInRight");
+    $(".projectInfo").css("display", "flex");
+    $("#projectInfoHeader").text("Tabbatha Plomaritas Portfolio");
+    $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/plomaritasportfolio");
+    $("#viewOnWeb").css("display", "none");
+    $("#projectDescript").text("My current web development portfolio is currently a work in progress and being refined daily. I am using Flexbox, CSS animations, JavaScript, and jQuery as I conintue to build this project. I am using this website to explore and practice the capabilities of SASS.");  
   }, 500); 
 
   setTimeout(function(){
@@ -52,8 +68,7 @@ $("#rgb").click(function(){
       }, 1000);
 });
 
-
-
+//todo game
 $("#todo").click(function(){
   setTimeout(function(){
     $(".projectInfo").addClass("moveInRight");
@@ -67,6 +82,58 @@ $("#todo").click(function(){
     $(".projectInfo").removeClass("moveInRight");
   }, 1000);
 });
+
+//javascript exercises
+$("#js").click(function(){
+  setTimeout(function(){
+    $(".projectInfo").addClass("moveInRight");
+    $(".projectInfo").css("display", "flex");
+    $("#projectInfoHeader").text("JavaScript Exercises");
+    $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/PlomaritasJavaScriptExercises");
+    $("#viewOnWeb").attr("href", "https://tabbplomaritas.github.io/PlomaritasJavaScriptExercises/");
+    $("#projectDescript").text("A series of JavaScript challenges I was required to complete for entrance into the Grand Circus Front-End Bootcamp.");  
+}, 500);  
+  setTimeout(function(){
+    $(".projectInfo").removeClass("moveInRight");
+  }, 1000);
+});
+
+//GearShare
+$("#gearshare").click(function(){
+  setTimeout(function(){
+    $(".projectInfo").addClass("moveInRight");
+    $(".projectInfo").css("display", "flex");
+    $("#projectInfoHeader").text("Gear Share");
+    $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/GearShare");
+    $("#viewOnWeb").attr("href", "https://tabbplomaritas.github.io/GearShare/");
+    $("#projectDescript").text("Gear Share is a fictitious start-up company that I created for practicing and exploring web development tools as I learn them. This application will connect users of audio and photo technical equipment to other users for loaning and renting gear. It currently utilizes SASS and JavaScript but will soon showcase the use of a variety of technologies.");  
+}, 500);  
+  setTimeout(function(){
+    $(".projectInfo").removeClass("moveInRight");
+  }, 1000);
+});
+
+//tabbPhotos
+$("#tabbphotos").click(function(){
+  setTimeout(function(){
+    $(".projectInfo").addClass("moveInRight");
+    $(".projectInfo").css("display", "flex");
+    $("#projectInfoHeader").text("tabbPhotos");
+    $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/tabbphotos");
+    $("#viewOnWeb").attr("href", "https://tabbplomaritas.github.io/tabbphotos/");
+    $("#projectDescript").html("This project is a landing page for a portrait and wedding photographer. One of my earliest projects, it utilizes Bootstrap, Media Queries, sticky positioning, all contained in a well designed one page layout. Bonus fact: Tabbatha Plomaritas <em>is</em> tabbPhotos!");  
+}, 500);  
+  setTimeout(function(){
+    $(".projectInfo").removeClass("moveInRight");
+  }, 1000);
+});
+
+
+
+
+
+
+
 
 
 
