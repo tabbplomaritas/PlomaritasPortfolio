96dba653622832e6026c7b3131f6e14507fe6c9c
tabbatharenea@gmail.com <tabbatharenea@gmail.com>
2018-05-12 16:13:07 -0400

    continued to update clicks on portfolio images in the javascript

--- js/portfolio.js
+++ js/portfolio.js
@@ -14,7 +14,7 @@ $("#closeProjectWindow").click(function(){
   setTimeout(function(){
     $(".projectInfo").css("display", "none");
     $(".projectInfo").removeClass("animated slideOutRight-50");
-    // $("#viewOnWeb").css("display", "block");
+
   }, 1050);
 }); 
 
@@ -29,6 +29,7 @@ let projectHeader = "";
 let gitCode = "";
 let gitPage = "";
 let projectText = "";
+let  projectImg = "";
 let clickedThumb = $(this)[0].id;
 //this is to test that we are grabbing correct value
 // console.log(clickedThumb); 
@@ -48,31 +49,105 @@ let clickedThumb = $(this)[0].id;
   //   projectText = "My web development portfolio is currently a work in progress and being refined daily. I am using Flexbox, CSS animations, JavaScript, and jQuery as I conintue to build this project. I am using this website to explore and practice the capabilities of SASS, jQuery, and other super fun front-end development tools!";
   // }
 
+  //template to add new projectinfo case
+
+  // case "portfolioThumb":
+    
+    // projectHeader = "";
+    // gitCode = "";
+    // gitPage ="";
+    // projectText = "";
+    // projectImg = ""
+  // break;
+
+
   switch(clickedThumb){
+    case "divvy":
+    
+      projectHeader = "Divvy",
+      gitCode = "https://github.com/tabbplomaritas/divvycopy",
+      gitPage ="https://tabbplomaritas.github.io/divvycopy/",
+      projectText = "Divvy is a mobile-first budgeting website, built over the course of two days at Grand Circus as a midterm project. This was my first time planning and coding as a member of a small team. Besides the JavScript and jQuery problem solving I was able to practice, my biggest gain from this project was learning from the group dynamic. I had a ton of fun planning, designing, testing, troubleshooting, and completing Divvy with my team.";
+      projectImg = "../images/projectThumbs/divvy.jpg"
+
+    break;
+
     case "rockpaper":
-      console.log("rockpaper is the clicked item."); 
+      
       projectHeader = "Rock Paper Scissors",
       gitCode = "https://github.com/tabbplomaritas/rock-paper-scissors",
       gitPage ="https://tabbplomaritas.github.io/rock-paper-scissors/",
       projectText = "A classic game of Rock, Paper, Scissors using Javascript and jQuery. Includes fun CSS transitions, animations, and sound effects!";
+      projectImg = "../images/projectThumbs/rockpaper.jpg"
     break;
 
     case "portfolioThumb":
-      console.log("it's portfolio time!");
+    
       projectHeader = "Tabbatha Plomaritas Portfolio";
       gitCode = "https://github.com/tabbplomaritas/plomaritasportfolio";
       gitPage ="http://www.tabbatha.net";
       projectText = "My web development portfolio is currently a work in progress and being refined daily. I am using Flexbox, CSS animations, JavaScript, and jQuery as I conintue to build this project. I am using this website to explore and practice the capabilities of SASS, jQuery, and other super fun front-end development tools!";
+      projectImg = "../images/projectThumbs/portfolio.jpg"
+    break;
+
+    case "gearshare":
+    
+      projectHeader = "GearShare";
+      gitCode = "https://github.com/tabbplomaritas/GearShare";
+      gitPage ="https://tabbplomaritas.github.io/GearShare/";
+      projectText = "Gear Share is a fictitious start-up company that I created for practicing and exploring web development tools as I learn them. This application will connect users of audio and photo technical equipment to other users for loaning and renting gear. It currently utilizes SASS and JavaScript but will soon showcase the use of a variety of technologies.";
+      projectImg = "../images/projectThumbs/gearshare.jpg"
+    break;
+
+    case "addressBook":
+      
+      projectHeader = "Address Book";
+      gitCode = "https://github.com/tabbplomaritas/addressbook_DOM";
+      gitPage ="https://tabbplomaritas.github.io/addressBook_DOM/";
+      projectText = "A pure JavaScript address book that allows users to use a form to add new contacts and delete dynamically created contacts.";
+      projectImg = "../images/projectThumbs/addressBook.jpg";
+    break;
+
+    case "rgb":
+      
+      projectHeader = "The Great RGB Game";
+      gitCode = "https://github.com/tabbplomaritas/Color-Game";
+      gitPage ="https://tabbplomaritas.github.io/Color-Game/";
+      projectText = "This vanilla-JavaScript-only guessing game will help you hone your RGB skills! Building this game was a lot of fun and added to my understanding of looping through arrays, using Math.random to generate random colors, and 'this'.";
+      projectImg = "../images/projectThumbs/rgb.jpg"
+    break;
+
+    case "todo":
+    
+      projectHeader = "To-Do List";
+      gitCode = "https://github.com/tabbplomaritas/ToDo-List";
+      gitPage ="https://tabbplomaritas.github.io/ToDo-List/";
+      projectText = "An interactive Todo List app that uses JavaScript and jQuery.";
+      projectImg = "../images/projectThumbs/todo.jpg";
+    break;
+
+
+
+    $(".projectInfo").addClass("moveInRight");
+    $(".projectInfo").css("display", "flex");
+    $("#projectInfoHeader").text("To-do List");
+    $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/ToDo-List");
+    $("#viewOnWeb").attr("href", "https://tabbplomaritas.github.io/ToDo-List/");
+    $("#projectDescript").text("An interactive Todo List app that uses JavaScript and jQuery.");  
+
+
+ 
   }
   console.log(projectHeader);
 
-//put those vairables into action and populate the project info popup
+//put those vairables into action and populate the project info window
     $(".projectInfo").addClass("animated slideInRight-50");
     $(".projectInfo").css("display", "flex");
     $("#projectInfoHeader").text(projectHeader);
     $("#viewOnGit").attr("href", gitCode);
     $("#viewOnWeb").attr("href", gitPage);
     $("#projectDescript").text(projectText);  
+    $("#projectInfoImg").attr("src", projectImg);
 
 // remove that animation class so that next time they click it runs again
   setTimeout(function(){
@@ -80,102 +155,16 @@ let clickedThumb = $(this)[0].id;
   }, 1000);
 });
 
-//portfolio
-// $("#portfolioThumb").click(function(){
-//   setTimeout(function(){
-//     $(".projectInfo").addClass("moveInRight");
-//     $(".projectInfo").css("display", "flex");
-//     $("#projectInfoHeader").text("Tabbatha Plomaritas Portfolio");
-//     $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/plomaritasportfolio");
-//     $("#viewOnWeb").css("display", "none");
-//     $("#projectDescript").text("My web development portfolio is currently a work in progress and being refined daily. I am using Flexbox, CSS animations, JavaScript, and jQuery as I conintue to build this project. I am using this website to explore and practice the capabilities of SASS.");  
-//   }, 500); 
-
-//   setTimeout(function(){
-//     $(".projectInfo").removeClass("moveInRight");
-//   }, 1000);
-// });
-
-//rgb game
-$("#rgb").click(function(){
-  setTimeout(function(){
-    $(".projectInfo").addClass("moveInRight");
-    $(".projectInfo").css("display", "flex");
-    $("#projectInfoHeader").text("The Great RGB Game");
-    $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/Color-Game");
-    $("#viewOnWeb").attr("href", "https://tabbplomaritas.github.io/Color-Game/");
-    $("#projectDescript").text("This guessing game will help you hone your RGB skills! Uses JavaScript, jQuery, and randomized RGB colors."); 
-  }, 500);   
-
-  setTimeout(function(){
-    $(".projectInfo").removeClass("moveInRight");
-      }, 1000);
-});
-
-//todo game
-$("#todo").click(function(){
-  setTimeout(function(){
-    $(".projectInfo").addClass("moveInRight");
-    $(".projectInfo").css("display", "flex");
-    $("#projectInfoHeader").text("To-do List");
-    $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/ToDo-List");
-    $("#viewOnWeb").attr("href", "https://tabbplomaritas.github.io/ToDo-List/");
-    $("#projectDescript").text("An interactive Todo List app that uses JavaScript and jQuery.");  
-}, 500);  
-  setTimeout(function(){
-    $(".projectInfo").removeClass("moveInRight");
-  }, 1000);
-});
-
-//javascript exercises
-$("#js").click(function(){
-  setTimeout(function(){
-    $(".projectInfo").addClass("moveInRight");
-    $(".projectInfo").css("display", "flex");
-    $("#projectInfoHeader").text("JavaScript Exercises");
-    $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/PlomaritasJavaScriptExercises");
-    $("#viewOnWeb").attr("href", "https://tabbplomaritas.github.io/PlomaritasJavaScriptExercises/");
-    $("#projectDescript").text("A series of JavaScript challenges I was required to complete for entrance into the Grand Circus Front-End Bootcamp.");  
-}, 500);  
-  setTimeout(function(){
-    $(".projectInfo").removeClass("moveInRight");
-  }, 1000);
-});
-
-//GearShare
-$("#gearshare").click(function(){
-  setTimeout(function(){
-    $(".projectInfo").addClass("moveInRight");
-    $(".projectInfo").css("display", "flex");
-    $("#projectInfoHeader").text("Gear Share");
-    $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/GearShare");
-    $("#viewOnWeb").attr("href", "https://tabbplomaritas.github.io/GearShare/");
-    $("#projectDescript").text("Gear Share is a fictitious start-up company that I created for practicing and exploring web development tools as I learn them. This application will connect users of audio and photo technical equipment to other users for loaning and renting gear. It currently utilizes SASS and JavaScript but will soon showcase the use of a variety of technologies.");  
-}, 500);  
-  setTimeout(function(){
-    $(".projectInfo").removeClass("moveInRight");
-  }, 1000);
-});
-
-
-//addressbook
 
 
 
-$("#addressBook").click(function(){
-  setTimeout(function(){
-    $(".projectInfo").addClass("moveInRight");
-    $(".projectInfo").css("display", "flex");
-    $("#projectInfoHeader").text("Address Book");
-    $("#viewOnGit").attr("href", "https://github.com/tabbplomaritas/addressbook_DOM");
-    $("#viewOnWeb").attr("href", "https://tabbplomaritas.github.io/addressBook_DOM/");
-    $("#projectDescript").text("A pure JavaScript address book that allows users to use a form to add new contacts and delete dynamically created contacts.");  
-  }, 500); 
+ 
 
-  setTimeout(function(){
-    $(".projectInfo").removeClass("moveInRight");
-  }, 1000);
-});
+
+
+
+
+
 
 
 //tabbPhotos
